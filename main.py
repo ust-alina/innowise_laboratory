8e8da48e2d8f1139c23cbaff9292ce5363eb1dce
ust-alina <ust.alina01@gmail.com>
2025-11-20 10:17:05 +0300

    Add lecture_2

---
+++ main.py
@@ -0,0 +1,48 @@
+def generate_profile(age: int):
+    if 0 <= age <= 12:
+        return "Child"
+    elif 13 <= age <= 19: 
+        return "Teenager"
+    elif age >= 20: 
+        return "Adult"
+    else:
+        return "Age not found"
+
+user_name = input("Hi! Enter your full name: ")
+birth_year_str = input("Enter your birth year: ")
+
+birth_year = int(birth_year_str)
+
+current_age = 2025 - birth_year
+
+hobbies = []
+hobby = input("Enter a favorite hobby or type 'stop' to finish: ")
+
+while hobby.lower() != 'stop':
+    hobbies.append(hobby)
+    hobby = input("Enter a favorite hobby or type 'stop' to finish: ")
+
+
+life_stage = generate_profile(current_age)
+
+
+user_profile = {
+    "name": user_name,
+    "age": current_age,
+    "stage": life_stage,
+    "hobbies": hobbies
+}
+
+print("---")
+print("Profile Summary:")
+print(f"Name: {user_profile['name']}")
+print(f"Age: {user_profile['age']}")
+print(f"Life Stage: {user_profile['stage']}")
+
+if len(hobbies) == 0:
+   print("You didn't mention any hobbies.")
+else:
+    print(f"Favorite Hobbies ({len(hobbies)}):")
+    for hobby in hobbies:
+        print(f"- {hobby}")
+print("---")
